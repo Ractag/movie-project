f545997cf7443900dad15ceacc68942164872710
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-10-16 21:45:01 +0200

    Star Rating package added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import ReactStars from "react-rating-stars-component";
 
 const MovieCard = ({ film }) => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
   return (
     <div className="movie-container">
       <h2>{film.title}</h2>
       <img className="movie-img" src={film.image} alt={film.name} />
       <p>{film.description}</p>
+      <div className="stars-container">
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={40}
+          activeColor="#ffd700"
+        />,
+      </div>
     </div>
   );
 };

8f6943b40d35cb0323c5c02cf6a15c8f03ea9b1a
Leslie Abajoli <leslie.abajoli1989@gmail.com>
2023-10-16 12:31:02 +0200

    Footer creation

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+
+    return (
+        <footer>Claire, Fréd, Leslie and Victor.
+            © 2008-2021 ViLeClaF. All rights reserved. </footer>
+    )
+
+}
+
+export default Footer;
